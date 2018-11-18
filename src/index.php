beda46cefc1dec70443272d226aefb4339e1bc0c
backzacc <backzacc@backzacc.ch>
2018-11-18 19:44:29 +0100

    Create index.php

---
+++ src/index.php
@@ -0,0 +1,69 @@
+
+<!doctype html>
+<html>
+<head>
+	<title>
+		Backzacc
+	</title>
+<script src="script.js"></script>
+	<link  rel="stylesheet"href="style.css">
+<link rel="shortcut icon" href="https://backzacc.ch/favicon.ico" type="image/x-icon">
+<link rel="icon" href="https://backzacc.ch/favicon.ico" type="image/x-icon">
+<head>
+<body>
+	<main >
+		<div class='nav'>
+			<h1>Personal Links</h1>
+			<h1>a little bitter, a little sweet, a little gore</h1>
+			<h2>Stuff</h2>
+ 			<ul>
+  			<li><a target="_blank" href="https://www.twitch.tv/reckful">Rudeful</a></li>
+  			<li><a target="_blank" href="https://quizlet.com">Quizlet</a></li>
+  			<li><a target="_blank" href="https://dugi.moe/note/">dugi.moe</a></li>
+  			<li><a target="_blank" href="https://mail.cock.li">Mail</a></li>
+			</ul>
+			<h2>Storage</h2>
+ 			<ul>
+  			<li><a target="_blank" href="https://cloud.backzacc.ch">Cloud</a></li>
+  			<li><a target="_blank" href="http://kiriya.quickconnect.to/?launchApp=SYNO.SDS.Drive.Application">Moar Storage</a></li>
+  			<li><a target="_blank" href="ftp://kiriya.airdns.org:42374/looool/">4HEad ftp Storage</a></li>
+  			<li><a target="_blank" href="https://dediseedbox.com" >Seed</a></li>
+  			<li><a target="_blank" href="ftp://anonymous@weebftp.org/index.htm#home">WeebFTP</a></li>
+				<li><a target="_blank" href="https://nyaa.pantsu.cat">Nyaa</a></li>
+				<li><a target="_blank" href="https://tbzedu-my.sharepoint.com/">Sharepoint</a></li>
+			</ul> 
+			<h2>Normie Shit</h2>
+			<ul>
+  			<li><a target="_blank" href="https://twitch.tv/directory/following/live">Twitch</a></li>
+  			<li><a target="_blank" href="https://youtube.com/feed/subscriptions">Youtube</a></li>
+  			<li><a target="_blank" href="https://www.youtube.com/user/ruiruii96/videos">That seal pianist from YT</a></li>
+  			<li><a target="_blank" href="https://netflix.com">Netflix</a></li>
+  			<li><a target="_blank" href="https://twitter.com">Twitter</a></li>
+			</ul>
+			<h2 onclick="frameing()" >Forums</h2>
+			<ul>
+  			<li><a target="_blank" href="https://boards.4chan.org/g/">/g/ - Technology</a></li>
+  			<li><a target="_blank" href="https://boards.4chan.org/a/">/a/ - Anime</a></li>
+			</ul>
+			<h2>Wiki</h2>
+			<ul>	
+				<li><a target="_b:lank" href="https://wiki.archlinux.org/index.php/">Archwiki</a><li>
+
+			</ul>
+			<h2>Templeos</h2>
+			<ul>	
+				<li><a target="_b:lank" href="http://46.19.137.116:54031/webman/3rdparty/Virtualization/noVNC/vnc.html?autoconnect=true&reconnect=true&path=synovirtualization/ws/ee13be9e-0b2d-46c2-abcf-017dc3ed22a8&title=temple&kb_layout=de-ch&v=2.3.0-8722">TempleOS VM</a><li>
+
+			</ul>
+			<h2>Online Weebstuff</h2>
+			<ul>
+				<li><a target="_blank" href="http://novelonlinefree.com/">Light Novels</a></li>
+				<h3>Currently Reading</h3>
+				<ul><li><a target="_blank" href="http://novelonlinefree.com/novel/youkoso_jitsuryoku_shijou_shugi_no_kyoushitsu_e">Weeb</a></li></ul>
+			<ul>
+		</div>
+<!--<iframe id="lasd"  src="https://www.qwant.com/?client=qwant-firefox" ></iframe>-->
+	</main>
+
+</body>
+</html>
